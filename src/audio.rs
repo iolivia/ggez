e33a1fc5e996466d26fd3ddfb24b5e40fe9cbe49
Olivia <olivia_github@outlook.com>
2020-04-08 22:39:02 +0100

    Use internal audio implementation

--- src/audio.rs
+++ src/audio.rs
@@ -11,16 +11,187 @@ use std::mem;
 use std::path;
 use std::time;
 
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::sync::Arc;
 
 use mint;
 use rodio;
 
+use std::sync::mpsc::Receiver;
+use std::sync::Mutex;
+use std::time::Duration;
+
+use rodio::play_raw;
+use rodio::queue;
+use rodio::source::Done;
+use rodio::Device;
+use rodio::Sample;
+use rodio::Source as RodioSource;
+
 use crate::context::Context;
 use crate::error::GameError;
 use crate::error::GameResult;
 use crate::filesystem;
+
+/// Handle to an device that outputs sounds.
+///
+/// Dropping the `Sink` stops all sounds. You can use `detach` if you want the sounds to continue
+/// playing.
+pub struct Sink {
+    queue_tx: Arc<queue::SourcesQueueInput<f32>>,
+    sleep_until_end: Mutex<Option<Receiver<()>>>,
+
+    controls: Arc<Controls>,
+    sound_count: Arc<AtomicUsize>,
+
+    detached: bool,
+}
+
+impl fmt::Debug for Sink {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<Sink: {:p}>", self)
+    }
+}
+
+struct Controls {
+    pause: AtomicBool,
+    volume: Mutex<f32>,
+    stopped: AtomicBool,
+}
+
+impl Sink {
+    /// Builds a new `Sink`, beginning playback on a Device.
+    #[inline]
+    pub fn new(device: &Device) -> Sink {
+        let (sink, queue_rx) = Sink::new_idle();
+        play_raw(device, queue_rx);
+        sink
+    }
+
+    /// Builds a new `Sink`.
+    #[inline]
+    pub fn new_idle() -> (Sink, queue::SourcesQueueOutput<f32>) {
+        let (queue_tx, queue_rx) = queue::queue(true);
+
+        let sink = Sink {
+            queue_tx: queue_tx,
+            sleep_until_end: Mutex::new(None),
+            controls: Arc::new(Controls {
+                pause: AtomicBool::new(false),
+                volume: Mutex::new(1.0),
+                stopped: AtomicBool::new(false),
+            }),
+            sound_count: Arc::new(AtomicUsize::new(0)),
+            detached: false,
+        };
+        (sink, queue_rx)
+    }
+
+    /// Appends a sound to the queue of sounds to play.
+    #[inline]
+    pub fn append<S>(&self, source: S)
+    where
+        S: RodioSource + Send + 'static,
+        S::Item: Sample,
+        S::Item: Send,
+    {
+        let controls = self.controls.clone();
+
+        let source = source
+            .pausable(false)
+            .amplify(1.0)
+            .stoppable()
+            .periodic_access(Duration::from_millis(5), move |src| {
+                if controls.stopped.load(Ordering::SeqCst) {
+                    src.stop();
+                } else {
+                    src.inner_mut().set_factor(*controls.volume.lock().unwrap());
+                    src.inner_mut()
+                        .inner_mut()
+                        .set_paused(controls.pause.load(Ordering::SeqCst));
+                }
+            })
+            .convert_samples();
+        let _count = self.sound_count.fetch_add(1, Ordering::Relaxed);
+        let source = Done::new(source, self.sound_count.clone());
+        *self.sleep_until_end.lock().unwrap() = Some(self.queue_tx.append_with_signal(source));
+    }
+
+    /// Gets the volume of the sound.
+    ///
+    /// The value `1.0` is the "normal" volume (unfiltered input). Any value other than 1.0 will
+    /// multiply each sample by this value.
+    #[inline]
+    pub fn volume(&self) -> f32 {
+        *self.controls.volume.lock().unwrap()
+    }
+
+    /// Changes the volume of the sound.
+    ///
+    /// The value `1.0` is the "normal" volume (unfiltered input). Any value other than `1.0` will
+    /// multiply each sample by this value.
+    #[inline]
+    pub fn set_volume(&self, value: f32) {
+        *self.controls.volume.lock().unwrap() = value;
+    }
+
+    /// Resumes playback of a paused sink.
+    ///
+    /// No effect if not paused.
+    #[inline]
+    pub fn play(&self) {
+        self.controls.pause.store(false, Ordering::SeqCst);
+    }
+
+    /// Pauses playback of this sink.
+    ///
+    /// No effect if already paused.
+    ///
+    /// A paused sink can be resumed with `play()`.
+    pub fn pause(&self) {
+        self.controls.pause.store(true, Ordering::SeqCst);
+    }
+
+    /// Gets if a sink is paused
+    ///
+    /// Sinks can be paused and resumed using `pause()` and `play()`. This returns `true` if the
+    /// sink is paused.
+    pub fn is_paused(&self) -> bool {
+        self.controls.pause.load(Ordering::SeqCst)
+    }
+
+    /// Stops the sink by emptying the queue.
+    #[inline]
+    pub fn stop(&self) {
+        self.controls.stopped.store(true, Ordering::SeqCst);
+    }
+
+    /// Destroys the sink without stopping the sounds that are still playing.
+    #[inline]
+    pub fn detach(mut self) {
+        self.detached = true;
+    }
+
+    /// Sleeps the current thread until the sound ends.
+    #[inline]
+    pub fn sleep_until_end(&self) {
+        if let Some(sleep_until_end) = self.sleep_until_end.lock().unwrap().take() {
+            let _ = sleep_until_end.recv();
+        }
+    }
+
+    /// Returns true if this sink has no more sounds to play.
+    #[inline]
+    pub fn empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Returns the number of sounds currently in the queue.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.sound_count.load(Ordering::Relaxed)
+    }
+}
 
 /// A trait object defining an audio context, allowing us to someday
 /// use something other than `rodio` if we really want.
@@ -279,7 +450,7 @@ impl SourceState {
 // code has done the data-slurping-from-zip's for us
 // but for now it works.
 pub struct Source {
-    sink: rodio::Sink,
+    sink: Sink,
     state: SourceState,
 }
 
@@ -298,7 +469,7 @@ impl Source {
                 "Could not decode the given audio data".to_string(),
             ));
         }
-        let sink = rodio::Sink::new(&context.audio_context.device());
+        let sink = Sink::new(&context.audio_context.device());
         let cursor = io::Cursor::new(data);
         Ok(Source {
             sink,
@@ -347,7 +518,7 @@ impl SoundSource for Source {
         self.play_later()?;
 
         let device = rodio::default_output_device().unwrap();
-        let new_sink = rodio::Sink::new(&device);
+        let new_sink = Sink::new(&device);
         let old_sink = mem::replace(&mut self.sink, new_sink);
         old_sink.detach();
 
@@ -389,7 +560,7 @@ impl SoundSource for Source {
         let volume = self.volume();
 
         let device = rodio::default_output_device().unwrap();
-        self.sink = rodio::Sink::new(&device);
+        self.sink = Sink::new(&device);
         self.state.play_time.store(0, Ordering::SeqCst);
 
         // Restore information from the previous link.
